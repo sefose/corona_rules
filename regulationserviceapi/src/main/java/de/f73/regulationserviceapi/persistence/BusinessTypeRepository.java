2d8115be1436ca6e198d2b5493aadd8a509d84f0
Hendrik Ohrdes <hendrikohrdes@root73server.fritz.box>
2020-09-01 10:38:38 +0200

    implemented CRUD for regulations

---
+++ regulationserviceapi/src/main/java/de/f73/regulationserviceapi/persistence/BusinessTypeRepository.java
@@ -0,0 +1,11 @@
+package de.f73.regulationserviceapi.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import de.f73.regulationserviceapi.models.BusinessType;
+
+@Repository
+public interface BusinessTypeRepository extends JpaRepository<BusinessType, Long> {
+    
+}
