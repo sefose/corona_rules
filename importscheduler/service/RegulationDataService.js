39a30dd630c9cfa3f86273614e6bb4582feadc4e
Robert Rudolph <robert@deepthought.local>
2020-09-01 19:57:26 +0200

    added status message to scheduler

--- importscheduler/service/RegulationDataService.js
+++ importscheduler/service/RegulationDataService.js
@@ -4,5 +4,6 @@ const STATE_NAME_URL = DATA_API_URL + "states";
 const REGULATION_URL = DATA_API_URL + "regulations";
 
 export const findAllRegulations = () => {
+  console.log("Start to load regulations");
   return axios.get(REGULATION_URL);
 };
