eaa421c74b4c45a341d1c4e625d5aaffb4df62cb
Robert Rudolph <robert@deepthought.local>
2020-09-01 20:37:00 +0200

    networking working

--- importdashboard/src/service/RegulationDataService.js
+++ importdashboard/src/service/RegulationDataService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const DATA_API_URL = "http://regulationdataserver:8081/";
+const DATA_API_URL = "http://localhost:8081/";
 const STATE_NAME_URL = DATA_API_URL + "states";
 const REGULATION_URL = DATA_API_URL + "regulations";
 
--- importscheduler/service/RegulationDataService.js
+++ importscheduler/service/RegulationDataService.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 const DATA_API_URL = "http://regulationdataserver:8081/";
-const STATE_NAME_URL = DATA_API_URL + "states";
 const REGULATION_URL = DATA_API_URL + "regulations";
 
 export const findAllRegulations = () => {
