23bfd95f3932a9bcd8adbf4f7671ebcb6987248c
Robert Rudolph <robertrudolph@Fakulta735siMac.fritz.box>
2020-09-01 11:00:26 +0200

    finshed state choice

--- importdashboard/src/components/StateNameDropdown.jsx
+++ importdashboard/src/components/StateNameDropdown.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
+import { findAllStateNames } from "../service/RegulationDataService";
 
-const GeneratedDropdown = ({ items, onChangeCallback }) => {
+const GeneratedDropdown = ({ onChangeCallback }) => {
+  const [stateNames, setStateNames] = useState([]);
+  
+  useEffect(() => {
+    findAllStateNames()
+      .then((resp) => {
+        console.log("resp", resp);
+        setStateNames(resp.data);
+      })
+      .catch((error) => console.log("error", error));
+  }, [setStateNames]);
+
   return (
-    <DropdownButton id="dropdown-basic-button" title="Dropdown button" onChange={onChangeCallback}>
-        {items.map(item => <Dropdown.Item>item</Dropdown.Item>)}
+    <DropdownButton
+      id="dropdown-basic-button"
+      title="Dropdown button"
+      onChange={onChangeCallback}
+    >
+      {stateNames.map((stateName) => (
+        <Dropdown.Item>stateName</Dropdown.Item>
+      ))}
     </DropdownButton>
   );
 };
