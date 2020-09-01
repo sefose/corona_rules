68f8a9216343e95ad58b9475fd7c0f6eaa3da128
Robert Rudolph <robertrudolph@Fakulta735siMac.fritz.box>
2020-09-01 13:16:12 +0200

    import working till dashboard

--- importdashboard/src/App.js
+++ importdashboard/src/App.js
@@ -1,12 +1,26 @@
-import React, {useState}from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import StateNameDropdown from "./components/StateNameDropdown";
-
+import {
+  findAllRegulations,
+  findRegulationByStateName,
+} from "./service/RegulationDataService";
 
 const App = () => {
+  const DEFAULTSTATECHOOSERTEXT = "please choose a State";
+  const [stateName, setStateName] = useState(DEFAULTSTATECHOOSERTEXT);
 
-  const [stateName, setStateName] = useState("please choose a State")
+  const importAllRegulations = () => {
+    findAllRegulations().then((resp) => console.log("resp.data", resp.data));
+  };
+
+  const importRegulationsByState = () => {
+    if (stateName !== DEFAULTSTATECHOOSERTEXT) {
+      findRegulationByStateName(stateName).then((resp) => console.log("resp.data", resp.data));
+    }
+  };
 
   return (
     <>
@@ -14,11 +28,21 @@ const App = () => {
         <Navbar.Brand>Import Dashbord</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Navbar.Text>get the data from the trustfull source</Navbar.Text>
+          <Navbar.Text>get the data from the trustful source</Navbar.Text>
         </Navbar.Collapse>
       </Navbar>
       <Container>
-        <StateNameDropdown onChangeCallback={setStateName} ></StateNameDropdown>
+        <div className="d-flex justify-content-between m-3">
+          <StateNameDropdown
+            onChangeCallback={setStateName}
+          ></StateNameDropdown>
+          <Button onClick={importRegulationsByState} variant="secondary">
+            Import data for selected state
+          </Button>
+          <Button onClick={importAllRegulations} variant="secondary">
+            Import data for all states
+          </Button>
+        </div>
       </Container>
     </>
   );
